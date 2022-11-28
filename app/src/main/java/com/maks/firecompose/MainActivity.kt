ddcc77c3f3f9c529a8504cd696f4fd9763efed0f
Mahesh <muley.mahesh@gmail.com>
2022-11-28 17:56:02 +0530

    Added Add product screen and navigation logic to switch between screens

--- app/src/main/java/com/maks/firecompose/MainActivity.kt
+++ app/src/main/java/com/maks/firecompose/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.maks.firecompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -11,135 +10,176 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.maks.firecompose.ui.theme.FireComposeTheme
 
 class MainActivity : ComponentActivity() {
+    val vm = ProductViewModel()
 
-    val list = mutableStateListOf<ProductViewModel.Product>()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FireComposeTheme {
+                var screenName by remember { mutableStateOf("Home") }
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
 
-                    Greeting(list)
+                    if(screenName == "Home")
+                    ProductListScreen(vm , { screenName = it })
+                    else
+                        AddProductScreen(vm , { screenName = it } )
 
                 }
             }
         }
-        ProductViewModel().getProducts { list.addAll(it) }
 
     }
+}
+
+@Composable
+fun ProductListScreen(vm: ProductViewModel, setScreen:(name:String) -> Unit) {
+    val productList = remember { mutableStateListOf<ProductViewModel.Product>() }
+
+    vm.getProducts { productList.addAll(it) }
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Row {
+                    Text("Firestore Demo")
+                }
+            })
+    },
+        content = {
+            Spacer(modifier = Modifier.padding(10.dp))
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                item {
+                    ProductListTitle()
+
+                }
+                items(productList) { product ->
+                    ProductItem(product = product)
+                }
+            }
+
+        }
+        , floatingActionButton = {
+            FloatingActionButton(
+                onClick = { setScreen("Add") },
+                contentColor = Color.White,
+            ) {
+                Icon(Icons.Filled.Add, "")
+            }
+        }
+    )
 
 }
 
 @Composable
-fun Greeting(platList: SnapshotStateList<ProductViewModel.Product>) {
+fun AddProductScreen(vm: ProductViewModel, setScreen: (name: String) -> Unit){
+    var name by remember { mutableStateOf("") }
+    var price by remember { mutableStateOf("0.0") }
 
-Scaffold(topBar = {
-    TopAppBar(
-        title = {
-            Row {
-                Text("Firestore Demo")
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Row {
+                    Text("Firestore Demo")
+                }
+            })
+    },
+        content = {
+            Spacer(modifier = Modifier.padding(10.dp))
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutlinedTextField( modifier = Modifier.fillMaxWidth(1f), value = name, onValueChange = { name = it}, label = {
+                    Text(text = "Product name")
+                })
+                OutlinedTextField(modifier = Modifier.fillMaxWidth(1f), value = price, onValueChange = { price = it }, label = {
+                    Text(text = "Price")
+                })
+                Button(onClick = { vm.addProduct(name,price.toDouble(),{ setScreen("Home")}) }) {
+                    Text(text = "Save")
+                }
             }
+
         })
-},
-    content = {
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-            item {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight()
-                        .padding(vertical = 5.dp),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        "\uD83C\uDF3F  Products",
-                        style = MaterialTheme.typography.h5
-                    )
-                }
-            }
-            item {
-                Row(
-                    modifier = Modifier
-                        .padding(1.dp)
-                        .background(MaterialTheme.colors.primary)
-                ) {
-                    Column(
-                        Modifier
-                            .weight(1f)
-                            .padding(8.dp)
-                    ) {
-                        Text(
-                            modifier = Modifier.fillMaxWidth(1f),
-                            text = " Product name",
-                            style = MaterialTheme.typography.h5,
-                            color = Color.White
+}
 
-                        )
-                    }
-                    Column(
-                        Modifier.padding(8.dp)
-                    ) {
-                        Text(
-                            "Price",
-                            style = MaterialTheme.typography.h5,
-                            color = Color.White
-                        )
-                    }
-                }
+
+@Composable
+fun ProductListTitle(){
+    Row(
+        modifier = Modifier
+            .padding(1.dp)
+            .background(MaterialTheme.colors.primary)
+    ) {
+        Column(
+            Modifier
+                .weight(1f)
+                .padding(8.dp)
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(1f),
+                text = " Product name",
+                style = MaterialTheme.typography.h6,
+                color = Color.White
+
+            )
+        }
+        Column(
+            Modifier.padding(8.dp)
+        ) {
+            Text(
+                "Price",
+                style = MaterialTheme.typography.h6,
+                color = Color.White
+            )
+        }
+    }
+}
+@Composable
+fun ProductItem(product: ProductViewModel.Product){
+    Card(
+        modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        shape = MaterialTheme.shapes.medium,
+        elevation = 5.dp,
+        backgroundColor = MaterialTheme.colors.surface
+    )  {
+        Row(modifier = Modifier.padding(8.dp)) {
+            Column(
+                Modifier.weight(1f)
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(1f),
+                    text = " ${product.name}",
+                    style = MaterialTheme.typography.body1
+                )
             }
-            items(platList) { product ->
-                Card(
-                    modifier = Modifier.padding(5.dp)
-                        .fillMaxWidth()
-                        .wrapContentHeight(),
-                    shape = MaterialTheme.shapes.medium,
-                    elevation = 5.dp,
-                    backgroundColor = MaterialTheme.colors.surface
-                )  {
-                    Row(modifier = Modifier.padding(8.dp)) {
-                        Column(
-                            Modifier.weight(1f)
-                        ) {
-                            Text(
-                                modifier = Modifier.fillMaxWidth(1f),
-                                text = " ${product.name}",
-                                style = MaterialTheme.typography.body1
-                            )
-                        }
-                        Column(
-                        ) {
-                            Text(
-                                " ${product.price}",
-                                style = MaterialTheme.typography.body1
-                            )
-                        }
-                    }
-                }
+            Column(
+            ) {
+                Text(
+                    " ${product.price}",
+                    style = MaterialTheme.typography.body1
+                )
             }
         }
     }
-)
 }
 
 @Preview(showBackground = true)
